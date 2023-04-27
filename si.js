bfaccd3122a9a6572a2c02fc145ad96144a28616
Ahmed Badr <lordahmed217@gmail.com>
2023-04-27 05:56:33 +0200

    update

---
+++ si.js
@@ -0,0 +1,51 @@
+let about = document.getElementById('about');
+
+
+
+   about.addEventListener('click', ()=>{
+        scrollTo(0 , 500)
+        
+    })
+
+    let team = document.getElementById('team');
+
+
+
+   team.addEventListener('click', ()=>{
+        scrollTo(0 , 1100)
+    })
+
+    let services = document.getElementById('services');
+
+
+
+    services.addEventListener('click', ()=>{
+        scrollTo(0 , 2000)
+    })
+
+    let blog = document.getElementById('blog');
+
+
+
+    blog.addEventListener('click', ()=>{
+        scrollTo(0 , 2670)
+    })
+
+    let contact = document.getElementById('contact');
+
+
+
+    contact.addEventListener('click', ()=>{
+        scrollTo(0 , 3550)
+    })
+
+    let butto = document.getElementById('butto');
+
+
+
+    butto.addEventListener('click', ()=>{
+        scrollTo(0 , 3550)
+    })
+
+   
+   
